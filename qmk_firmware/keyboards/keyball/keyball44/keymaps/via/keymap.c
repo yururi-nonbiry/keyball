a6fb0c54a77fae65f376afdadf537c34ca7ea29a
yururi-nonbiry <yururi.nonbiry@gmail.com>
2026-04-29 08:05:05 +0900

    feat: add VIA keymap implementation for Keyball44

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
-#include "keyball.h"
 
 enum custom_keycodes {
     MINS_UNDS = SAFE_RANGE,
